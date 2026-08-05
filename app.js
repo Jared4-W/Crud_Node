6446b8b58d10fc26e507f8feb28dd957130fef75
Jared <jaredchavezcelaya@gmail.com>
2026-08-04 21:20:02 -0600

    Configurar Railway

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');  //referenciar a express
 const app = express();  // se invoca express mediante su clase
-//const session = require('express-session');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');  //invocar motor de plantillas
@@ -11,7 +11,9 @@ app.use(express.urlencoded({extended:false}));//definir como guadar datos
 
 app.use(cookieParser());
 
-/* app.use(session({
+const PORT = process.env.PORT || 5000;
+
+app.use(session({
 
     secret:'control_escolar',
 
@@ -19,7 +21,7 @@ app.use(cookieParser());
 
     saveUninitialized:false
 
-})); */
+})); 
 
 app.use((req,res,next)=>{
 
@@ -33,6 +35,10 @@ app.use(express.json());
 
 app.use('/', require('./router'));  //refrenciar archivo router.js
 
-app.listen(5000, ()=>{    //invocar los metodos de express
-console.log('SERVER corriendo en http://localhost:5000');
-}); //mostrar mensaje para verificar el proceso junto con enlace
+// app.listen(5000, ()=>{    //invocar los metodos de express
+// console.log('SERVER corriendo en http://localhost:5000');
+// }); //mostrar mensaje para verificar el proceso junto con enlace
+
+app.listen(PORT, () => {  //render
+  console.log(`SERVER corriendo en puerto ${PORT}`);
+});
--- database/db.js
+++ database/db.js
@@ -1,5 +1,40 @@
 const mysql = require('mysql2');
 
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+module.exports = pool;
+
+/* const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+conexion.connect((error)=>{
+    if(error){
+        console.log('Error de conexión:', error);
+        return;
+    }
+
+    console.log('Base de datos conectada');
+});
+
+module.exports = conexion; */
+
+/* const mysql = require('mysql2');
+
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -16,4 +51,4 @@ conexion.connect((error)=>{
     console.log('¡Conectado a control_escolar!');
 });
 
-module.exports = conexion;
+module.exports = conexion; */
